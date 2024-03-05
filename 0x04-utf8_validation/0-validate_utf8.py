0c1b82a4874fd62ebaee32f0a1c1eed6a8d76a2d
Ebelechukwu Muoka <128797779+Ebele21@users.noreply.github.com>
2024-03-05 00:30:47 -0800

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+""" This is the solution for UTF-8 Validation """
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
+    This Method that determines if a given data set represents a valid
     UTF-8 encoding.
     """
     number_bytes = 0
@@ -38,4 +38,3 @@ def validUTF8(data):
         return True
 
     return False
-
